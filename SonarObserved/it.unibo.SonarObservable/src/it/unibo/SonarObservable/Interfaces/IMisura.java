d86ccc3ff4ee5ae7c4aefe5df0a00877f209a860
MJG07 <77410390+MJG07@users.noreply.github.com>
2022-04-06 23:59:39 +0200

    Delete SonarObserved directory

--- SonarObserved/it.unibo.SonarObservable/src/it/unibo/SonarObservable/Interfaces/IMisura.java
+++
@@ -1,8 +0,0 @@
-package it.unibo.SonarObservable.Interfaces;
-
-import it.unibo.radarSystem22.domain.interfaces.IDistance;
-
-public interface IMisura extends IDistance {
-	public void setValue(IDistance d);
-	public IDistance getDistance();
-}
